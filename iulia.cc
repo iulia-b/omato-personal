35fd30cca760dd6a5b9bfd533c169903652ef139
iulia bejan <iubejan@microsoft.com>
2019-05-21 18:50:36 +0200

    compiles

--- iulia.cc
+++ iulia.cc
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 
 struct Item {
   int brig;
@@ -23,23 +23,23 @@ void interpolate(int start, int end, int intervalMS) {
 
   while(iterator != finish) {
     // Serial.print(iterator);   // analogWrite(led, LedState);
+
+    // i used prin here so i can write the values in the console. you should use the line above to write them in the controller
     printf("%d\n", iterator);
+    
     iterator = iterator + step;
-    //delay(sleepTime);
+    delay(sleepTime); // for me delay does not work, i used sleep. delay should work for you
   }
 }
 
 
-void loop(Item *entries, int n) {
+void loop(struct Item *entries, int n) {
 
   int i = 1;
-  Item *current = (Item*)malloc(sizeof(Item));
 
   while (1 == 1) {
-    current = &entries[i];
-
-    interpolate(current[i-1].brig, current[i].brig, current[i-1].fade);
-    //delay(current[i-1].wait);
+    interpolate(entries[i-1].brig, entries[i].brig, entries[i-1].fade);
+    delay(entries[i-1].wait); // again here i used sleep cause delay does not work
 
     if(i == n) {
       i = 1;
@@ -57,13 +57,13 @@ int main() {
     // rampInt myRamp;                              // new ramp object (ramp<unsigned char> by default)
 
 
-    int n = 10;
-    //Item *entries = (Item*)malloc(sizeof(Item) * (n+1));
-    Item entries[10] = {{10, 10000, 10000}, {0, 0, 0}, {255, 10000, 5000}};
+    int n = 10; //declare how many items you have
+    
+    // make sure andries will be declared like entries[11] if n=10 or entries n+1 in general.
+    // Eg: for n = 10, put all your 10 values in entries and in the last position put again the first value. so entries will begin and start with the same value
+    struct Item entries[10] = {{10, 10000, 10000}, {0, 0, 0}, {255, 10000, 5000}};
 
-
-    //entries[n+1] = {10, 10000, 10000}];
-
+    loop(entries, 3);
 
 	return 0;
 }
